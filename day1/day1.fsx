7d0337627d9f20f3f7137a190dbc6bea620d9551
Andre Dublin <81dublin@gmail.com>
2015-12-12 16:25:16 -0500

    use a discriminated union to prevent edge cases
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- day1/day1.fsx
+++ day1/day1.fsx
@@ -1,17 +1,22 @@
 open System
 open System.IO
 
+type Movement =
+  | Up
+  | Down
+
 let file = File.ReadAllLines("input.txt")
-let charArray = Array.ofSeq (String.Join(String.Empty, file))
+let directions = Array.ofSeq (String.Join(String.Empty, file)) |> Array.map (fun x -> if x = '(' then Up else Down)
 let result =
-  charArray
+  directions
   |> Array.fold (fun (floor, basementFloor, position) elem ->
-    if elem = '(' then
+  match elem with
+  | Up ->
       if floor = -1 && basementFloor = -1 then
         (floor + 1, position, position + 1)
       else
         (floor + 1, basementFloor, position + 1)
-    else
+  | Down ->
       if floor = -1 && basementFloor = -1 then
         (floor - 1, position, position + 1)
       else
