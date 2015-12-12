6afefaea8d3a9a3260a624e5855333dc7bf458b8
Andre Dublin <81dublin@gmail.com>
2015-12-12 15:38:17 -0500

    change variable name
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- day1/day1.fsx
+++ day1/day1.fsx
@@ -2,6 +2,6 @@ open System
 open System.IO
 
 let file = File.ReadAllLines("input.txt")
-let charArray =
+let result =
   Array.ofSeq (String.Join(String.Empty, file))
   |> Array.fold (fun acc elem -> if elem = '(' then acc + 1 else acc - 1) 0
