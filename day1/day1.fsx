942c3238f36d1e57fbf324836414db5c25ae9bc4
Andre Dublin <81dublin@gmail.com>
2015-12-12 15:36:08 -0500

    a more idomatic fsharp solution
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- day1/day1.fsx
+++ day1/day1.fsx
@@ -1,8 +1,7 @@
 open System
 open System.IO
 
-let mutable floor = 0
 let file = File.ReadAllLines("input.txt")
-let charArray = Array.ofSeq (String.Join(String.Empty, file))
-for i in charArray do
-  if i = '(' then floor <- floor + 1 else floor <- floor - 1
+let charArray =
+  Array.ofSeq (String.Join(String.Empty, file))
+  |> Array.fold (fun acc elem -> if elem = '(' then acc + 1 else acc - 1) 0
